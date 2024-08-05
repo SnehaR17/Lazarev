b058fd4b5a4bd1a13127aa2fe2a63c3845d71112
SnehaR17 <1ds22cb050@dsce.edu.in>
2024-08-06 01:48:28 +0530

    js done

--- script.js
+++ script.js
@@ -39,5 +39,44 @@ function navAnimation() {
     })
 }
 
-navAnimation()
+// navAnimation()
 
+
+function page2Animation(){
+    var rightElems = document.querySelectorAll(".right-elem")
+
+    rightElems.forEach(function (elem) {
+        elem.addEventListener("mouseenter", function () {
+            gsap.to(elem.childNodes[3], {
+                opacity: 1,
+                scale: 1
+            })
+        })
+        elem.addEventListener("mouseleave", function () {
+            gsap.to(elem.childNodes[3], {
+                opacity: 0,
+                scale: 0
+            })
+        })
+        elem.addEventListener("mousemove", function (dets) {
+            gsap.to(elem.childNodes[3], {
+                x: dets.x - elem.getBoundingClientRect().x - 50,
+                y: dets.y - elem.getBoundingClientRect().y - 150
+            })
+        })
+    })
+}
+
+// page2Animation()
+
+var page3Center = document.querySelector(".page3-center")
+var video = document.querySelector("#page3 video")
+
+page3Center.addEventListener("click", function(){
+    video.play()
+    gsap.to(video, {
+        transform: "scaleX(1) scaleY(1)",
+        opacity: 1,
+        borderRadius: 0
+    })
+})
